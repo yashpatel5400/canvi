92014bf35cbb90807a3da92564eae516c52b576f
yashpatel5400 <ypatel5400@gmail.com>
2023-05-01 08:45:58 -0400

    Add calibration

--- canvi/methods/alternate.py
+++ canvi/methods/alternate.py
@@ -17,18 +17,19 @@ from sbi.inference import SNRE, SNLE, SNPE, BNRE
 plt.rcParams['text.usetex'] = True
 
 # rounds is for refinement (if using sequential alg; if not, set = 0) -- inference must be passed in if doing sequential
-def assess_coverage(task, amortized_posterior, fn, requires_mcmc, rounds = 0, inference = None, device = "cpu", coverage_trials = 1_000, num_coverage_pts = 20):
+def assess_coverage(task, amortized_posterior, fn, requires_mcmc, rounds = 0, inference = None, device = "cpu", coverage_trials = 2, num_coverage_pts = 20):
     calibration_prior = task.get_prior()
     calibration_simulator = task.get_simulator()
 
-    variational_coverages = np.zeros(num_coverage_pts)
     desired_coverages = [(1 / num_coverage_pts) * k for k in range(num_coverage_pts)]
+    conformal_quantiles = np.array([1 / np.quantile(cal_scores, q = coverage) for coverage in desired_coverages])
     
     # non-amortized methods (that require MCMC) are sooo slow: can only manageably run these very small sample sizes
     if requires_mcmc:
         sample_sizes = [32 * 2 ** j for j in range(5)]
     else:
         sample_sizes = [1024 * 2 ** j for j in range(8)]
+    conformal_coverages = np.zeros(num_coverage_pts)
     variational_coverages_per_size = [np.zeros(num_coverage_pts) for _ in sample_sizes]
     times = [[] for _ in sample_sizes]
 
@@ -45,7 +46,8 @@ def assess_coverage(task, amortized_posterior, fn, requires_mcmc, rounds = 0, in
                 sample_sizes = 32
             else:
                 sample_sizes = 1024
-            theta = proposal.sample((num_sims,))
+            refinement_sims = 100
+            theta = proposal.sample((refinement_sims,))
             x = calibration_simulator(theta)
             # In `SNLE` and `SNRE`, you should not pass the `proposal` to `.append_simulations()`
             if isinstance(inference, SNLE) or isinstance(inference, SNRE):
@@ -61,7 +63,8 @@ def assess_coverage(task, amortized_posterior, fn, requires_mcmc, rounds = 0, in
         posterior.set_default_x(test_x[0])
         predicted_lps = posterior.log_prob(test_theta[0].view(1,-1).to(device)).detach()
         predicted_prob = predicted_lps.cpu().exp().numpy()
-        
+
+        conformal_coverages += predicted_prob > conformal_quantiles
         for i, sample_size in enumerate(sample_sizes):
             calibration_time_start = time.time()
             empirical_theta_dist = posterior.sample((sample_size,))
@@ -77,7 +80,8 @@ def assess_coverage(task, amortized_posterior, fn, requires_mcmc, rounds = 0, in
             times[i].append(training_time + calibration_time)
         print(f"Completed trial: {j}")
     plt.close()
-    
+
+    conformal_coverages /= coverage_trials
     dfs = []
     for i in range(len(variational_coverages_per_size)):
         df = pd.DataFrame(columns=["confidence", "coverages", "times", "sample_sizes",])
@@ -90,8 +94,9 @@ def assess_coverage(task, amortized_posterior, fn, requires_mcmc, rounds = 0, in
 
     sns.set_theme()
     sns.lineplot(data=df, x="confidence", y="coverages", hue="sample_sizes", palette="flare", legend="full")
-    sns.lineplot(data=df, x="coverages", y="coverages", c="black", linestyle='--')
-        
+    sns.lineplot(data=df, x="confidence", y="confidence", c="black", linestyle='--')
+    sns.lineplot(x=desired_coverages, y=conformal_coverages, c="green", linestyle='--')
+    
     plt.xlabel("$\\mathrm{Confidence Level}$")
     plt.ylabel("$\\mathrm{Empirical Coverage}$")
     plt.legend()
@@ -110,6 +115,27 @@ def assess_coverage(task, amortized_posterior, fn, requires_mcmc, rounds = 0, in
     
     plt.tight_layout()
     plt.savefig(f"{fn}-time.png")
+
+def plot_posterior(task, posterior, fn):
+    observation_idx = 1
+    observation = task.get_observation(num_observation=observation_idx)
+    posterior.set_default_x(observation)
+    samples = posterior.sample((1000,))
+    
+    columns = [f"$\\theta_{i}$" for i in range(samples.shape[-1])]
+    reference_samples_all = task.get_reference_posterior_samples(num_observation=observation_idx)
+    reference_samples = reference_samples_all[np.random.choice(len(reference_samples_all), 1_000, replace=False)]
+    ref_df = pd.DataFrame(columns=columns, data=reference_samples)
+    ref_df["label"] = "reference"
+    posterior_df = pd.DataFrame(columns=columns, data=samples)
+    posterior_df["label"] = "approximation"
+    df = pd.concat([ref_df, posterior_df])
+
+    sns.set_theme()
+    sns.pairplot(df, hue="label", kind="kde", corner=True)
+    plt.title("Approximate Posteriors")
+    plt.savefig(f"{fn}-posterior.png")
+    plt.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -131,10 +157,10 @@ if __name__ == "__main__":
             posterior = pickle.load(f)
     else:
         rounds = 1
-        num_sims = 10_000
+        train_sims = 10_000
 
         proposal = prior
-        theta = proposal.sample((num_sims,))
+        theta = proposal.sample((train_sims,))
         x = simulator(theta)
         # In `SNLE` and `SNRE`, you should not pass the `proposal` to `.append_simulations()`
         if isinstance(inference, SNLE) or isinstance(inference, SNRE):
@@ -149,18 +175,20 @@ if __name__ == "__main__":
         with open(cache_fn, "wb") as f:
             pickle.dump(posterior, f)
 
+    plot_posterior(task, posterior, fn)
+
+    # perform calibration
+    print("Calibrating...")
+    calibration_sims = 1_000
+    calibration_theta = prior.sample((calibration_sims,))
+    calibration_x = simulator(calibration_theta)
+    cal_scores = []
+    for calibration_theta_pt, calibration_x_pt in zip(calibration_theta, calibration_x):
+        posterior.set_default_x(calibration_x_pt)
+        log_prob = posterior.log_prob(calibration_theta_pt).detach()
+        prob = log_prob.cpu().exp().numpy()
+        cal_scores.append(1 / prob)
+    cal_scores = np.array(cal_scores)
+
     requires_mcmc = type(inference) in [SNRE, SNLE, BNRE] # only SNPE does not require MCMC
     assess_coverage(task, posterior, fn, requires_mcmc, rounds=args.rounds, inference=inference)
-
-    observation = task.get_observation(num_observation=1)
-    posterior.set_default_x(observation)
-    samples = posterior.sample((1000,))
-    columns = [f"$\\theta_{i}$" for i in range(samples.shape[-1])]
-    posterior_df = pd.DataFrame(columns=columns, data=samples)
-    posterior_df["label"] = "approximation"
-
-    sns.set_theme()
-    sns.pairplot(posterior_df, hue="label", kind="kde", corner=True)
-    plt.title("Approximate Posteriors")
-    plt.savefig(f"{fn}-posterior.png")
-    plt.close()
