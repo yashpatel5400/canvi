fixed potential bug in reparameterization gradients

canvi/arch/losses.py
@@ -1,6 +1,6 @@
 import torch
 from generate import generate_data
-from utils import transform_parameters
+from utils import transform_parameters, transform_parameters_batch
 import torch.distributions as D
 import torch.nn as nn
 
@@ -10,19 +10,42 @@ def get_imp_weights(pts, num_samples, mdn=True, flow=False, log=False, prop_prio
     K = num_samples
     device = kwargs['device']
     log_target = kwargs['log_target']
-    log_prior = kwargs['log_prior']
+    log_prior_batch = kwargs['log_prior_batch']
 
-    particles, log_denoms = encoder.sample_and_log_prob(num_samples=K, context=pts.float().to(device))
-    particles = particles.reshape(K*mb_size, -1)
-    log_denoms = log_denoms.view(K,-1)
-    #repeated_pts = pts.repeat(K, 1, 1).reshape(K*mb_size, -1).to(device)
-    log_nums = log_prior(particles, **kwargs).reshape(K, mb_size).to(device) + log_target(particles, pts.to('cpu'), num_samples, **kwargs).reshape(K, mb_size).to(device)
+    particles = encoder.sample(K, pts.float().to(device))
+    rparticles = particles.reshape(K*mb_size, -1)
+    repeated_pts = pts.repeat(K, 1, 1).transpose(0,1)
+    repeated_pts = repeated_pts.reshape(K*mb_size, -1).to(device)
+    log_denoms = encoder.log_prob(rparticles, repeated_pts)
+    log_denoms = log_denoms.reshape(K, mb_size)
+    log_nums = log_prior_batch(particles, **kwargs).reshape(K, mb_size).to(device) + log_target(particles, pts.cpu(), K, **kwargs).reshape(K, mb_size).to(device)
+    #log_nums = log_target(particles, repeated_pts, **kwargs).reshape(K, mb_size)
     log_weights = log_nums - log_denoms
     weights = nn.Softmax(0)(log_weights)
     if log:
         return particles, weights, log_weights
     else:
         return particles, weights
+
+# def get_imp_weights(pts, num_samples, mdn=True, flow=False, log=False, prop_prior=0., **kwargs):
+#     mb_size = kwargs['mb_size']
+#     encoder = kwargs['encoder']
+#     K = num_samples
+#     device = kwargs['device']
+#     log_target = kwargs['log_target']
+#     log_prior = kwargs['log_prior']
+
+#     particles, log_denoms = encoder.sample_and_log_prob(num_samples=K, context=pts.float().to(device))
+#     particles = particles.reshape(K*mb_size, -1)
+#     log_denoms = log_denoms.view(K,-1)
+#     #repeated_pts = pts.repeat(K, 1, 1).reshape(K*mb_size, -1).to(device)
+#     log_nums = log_prior(particles, **kwargs).reshape(K, mb_size).to(device) + log_target(particles, pts.to('cpu'), num_samples, **kwargs).reshape(K, mb_size).to(device)
+#     log_weights = log_nums - log_denoms
+#     weights = nn.Softmax(0)(log_weights)
+#     if log:
+#         return particles, weights, log_weights
+#     else:
+#         return particles, weights
         
 def iwbo_loss(x, **kwargs):
     mb_size = kwargs['mb_size']

canvi/arch/main.py
@@ -57,8 +57,8 @@ def loss_choice(loss_name, x, **kwargs):
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
 def main(cfg : DictConfig) -> None:
-    # initialize(config_path=".", job_name="test_app")
-    # cfg = compose(config_name="config")
+    initialize(config_path=".", job_name="test_app")
+    cfg = compose(config_name="config")
     seed = cfg.seed
     torch.manual_seed(seed)
     random.seed(seed)
@@ -69,6 +69,9 @@ def main(cfg : DictConfig) -> None:
 
     dir = cfg.dir
     os.chdir(dir)
+
+    cfg.smc.K = 67
+    cfg.training.mb_size = 1
 
     (true_theta, 
     true_x, 

canvi/arch/setup.py
@@ -44,12 +44,12 @@ from generate import generate_data
 from losses import favi_loss, iwbo_loss, elbo_loss
 from scipy.integrate import quad
 from modules import TransformedUniform
-from utils import prior_t_sample, transform_parameters
+from utils import prior_t_sample, transform_parameters, transform_parameters_batch
 
 def log_target(particles, context, num_particles, **kwargs):
     T = kwargs['T']
     K = num_particles
-    params = transform_parameters(particles, **kwargs)
+    params = transform_parameters_batch(particles, **kwargs)
     theta1 = params[...,0]
     theta2 = params[...,1]
     eyes = torch.eye(T).repeat(K,1,1) #batch of Qs
@@ -61,7 +61,7 @@ def log_target(particles, context, num_particles, **kwargs):
     eps_vecs = torch.bmm(Qs, targets)
     eps_vecs = eps_vecs.squeeze(-1)
 
-    running_sum = torch.zeros(K)
+    running_sum = torch.zeros(1, K)
     for i in range(1, T):
         const = 1/torch.sqrt(2*math.pi*(.2+theta2*(eps_vecs[:,i-1]**2)))
         const = torch.log(const)
@@ -73,6 +73,12 @@ def log_target(particles, context, num_particles, **kwargs):
 def log_prior(particles, **kwargs):
     prior = kwargs['prior']
     theta = transform_parameters(particles, **kwargs)
+    lps = prior.log_prob(theta) #
+    return lps
+
+def log_prior_batch(particles, **kwargs):
+    prior = kwargs['prior']
+    theta = transform_parameters_batch(particles, **kwargs)
     lps = prior.log_prob(theta) #
     return lps
 
@@ -130,6 +136,7 @@ def setup(cfg):
         'K': K,
         'T': T,
         'log_prior': log_prior,
+        'log_prior_batch': log_prior_batch,
         'log_target': log_target,
         'proposal': proposal
     }