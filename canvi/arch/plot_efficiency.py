1281fb6cef3a4b51030aa57afc73d9863a3b417c
Declan McNamara <declan@umich.edu>
2023-05-22 09:54:36 -0400

    caching some stuff

--- canvi/arch/plot_efficiency.py
+++ canvi/arch/plot_efficiency.py
@@ -24,12 +24,14 @@ from scipy.interpolate import CubicSpline
 from os.path import exists
 import torch.distributions as D
 import torch
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import hydra
 from hydra import compose, initialize
 from omegaconf import DictConfig
 import random
+import json
 from cde.mdn import MixtureDensityNetwork
 from cde.nsf import build_nsf, EmbeddingNet
 import torch.nn as nn
@@ -37,6 +39,7 @@ from scipy.signal import savgol_filter
 from generate import generate_data
 from utils import posterior, normalizing_integral, transform_parameters
 from setup import setup
+from losses import lebesgue
 
 plt.style.use('ggplot')
 plt.rcParams.update({'font.size': 28})
@@ -76,8 +79,8 @@ def plot_eff():
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
 def main(cfg : DictConfig) -> None:
-    # initialize(config_path=".", job_name="test_app")
-    # cfg = compose(config_name="config")
+    initialize(config_path=".", job_name="test_app")
+    cfg = compose(config_name="config")
     seed = cfg.seed
     torch.manual_seed(seed)
     random.seed(seed)
@@ -95,6 +98,54 @@ def main(cfg : DictConfig) -> None:
     kwargs) = setup(cfg)
 
     plot_eff()
+    final_effs = {}
+
+    # IWBO
+    encoder.load_state_dict(torch.load('./weights/iwbo,0.0001,10.pth'))
+    calibration_theta, calibration_x = generate_data(1000, **kwargs)
+    lps = encoder.log_prob(calibration_theta, calibration_x).detach()
+    cal_scores = -1*lps.reshape(-1)
+    iwbo_effs = []
+    alphas = [.05]
+    for _ in range(10):
+        fresh_theta, fresh_x = generate_data(1000, **kwargs)
+        this_eff = [lebesgue(cal_scores, fresh_theta, fresh_x, alpha, **kwargs) for alpha in alphas]
+        iwbo_effs.append(this_eff)
+    iwbo_effs = np.stack(iwbo_effs).mean(-1)
+    iwbo_df = pd.DataFrame(iwbo_effs, columns=alphas)
+
+    # ELBO
+    encoder.load_state_dict(torch.load('./weights/elbo,0.0001,10.pth'))
+    calibration_theta, calibration_x = generate_data(1000, **kwargs)
+    lps = encoder.log_prob(calibration_theta, calibration_x).detach()
+    cal_scores = -1*lps.reshape(-1)
+    elbo_effs = []
+    for _ in range(1):
+        fresh_theta, fresh_x = generate_data(1000, **kwargs)
+        this_eff = [lebesgue(cal_scores, fresh_theta, fresh_x, alpha, **kwargs) for alpha in alphas]
+        elbo_effs.append(this_eff)
+    elbo_effs = np.stack(elbo_effs).mean(-1)
+    elbo_df = pd.DataFrame(elbo_effs, columns=alphas)
+
+    # FAVI
+    encoder.load_state_dict(torch.load('./weights/favi,0.0001,10.pth'))
+    calibration_theta, calibration_x = generate_data(1000, **kwargs)
+    lps = encoder.log_prob(calibration_theta, calibration_x).detach()
+    cal_scores = -1*lps.reshape(-1)
+    favi_effs = []
+    for _ in range(1):
+        fresh_theta, fresh_x = generate_data(1000, **kwargs)
+        this_eff = [lebesgue(cal_scores, fresh_theta, fresh_x, alpha, **kwargs) for alpha in alphas]
+        favi_effs.append(this_eff)
+    favi_effs = np.stack(favi_effs).mean(-1)
+    favi_df = pd.DataFrame(favi_effs, columns=alphas)
+
+    with open('./figs/final_eff_elbo.tex','w') as tf:
+        tf.write(elbo_df.to_latex())
+    with open('./figs/final_eff_iwbo.tex','w') as tf:
+        tf.write(iwbo_df.to_latex())
+    with open('./figs/final_eff_favi.tex','w') as tf:
+        tf.write(favi_df.to_latex())
 
 if __name__ == "__main__":
     main() 
